b43952b932f7c0bb065abe10b2f2ed013a763bef
Oliver-makes-code <oliver@olivermakesco.de>
2024-09-01 20:09:26 -0500

    Binary reader/writer

---
+++ Serialization/Reader/BinaryDataReader.cs
@@ -0,0 +1,74 @@
+namespace Greenhouse.Libs.Serialization.Reader;
+
+public record BinaryDataReader(BinaryReader Binary) : DataReader {
+    public ArrayDataReader Array()
+        => new ArrayReader(this, Primitive().Int());
+    public ObjectDataReader Object()
+        => new ObjectReader(this);
+    public PrimitiveDataReader Primitive()
+        => new PrimitiveReader(Binary);
+
+    private record PrimitiveReader(BinaryReader Binary) : PrimitiveDataReader {
+        public bool Bool()
+            => Binary.ReadBoolean();
+        public byte Byte()
+            => Binary.ReadByte();
+        public char Char()
+            => Binary.ReadChar();
+        public double Double()
+            => Binary.ReadDouble();
+        public float Float()
+            => Binary.ReadSingle();
+        public int Int()
+            => Binary.ReadInt32();
+        public long Long()
+            => Binary.ReadInt64();
+        public sbyte SByte()
+            => Binary.ReadSByte();
+        public short Short()
+            => Binary.ReadInt16();
+        public string String()
+            => Binary.ReadString();
+        public uint UInt()
+            => Binary.ReadUInt32();
+        public ulong ULong()
+            => Binary.ReadUInt64();
+        public ushort UShort()
+            => Binary.ReadUInt16();
+    }
+
+    private class ArrayReader(BinaryDataReader reader, int length) : ArrayDataReader {
+        private readonly BinaryDataReader Reader = reader;
+
+        public override void End() {}
+        public override int Length()
+            => length;
+        public override DataReader Value() 
+            => Reader;
+    }
+
+    private class ObjectReader(BinaryDataReader reader) : ObjectDataReader {
+        private readonly BinaryDataReader Reader = reader;
+
+        public override void End() {}
+
+        public override DataReader Field(string name)
+            => Reader;
+
+        public override NullableFieldDataReader NullableField(string name)
+            => new NullableFieldReader(Reader, Reader.Primitive().Bool());
+    }
+
+    private class NullableFieldReader(BinaryDataReader reader, bool hasValue) : NullableFieldDataReader {
+        private readonly BinaryDataReader Reader = reader;
+        private readonly bool HasValue = hasValue;
+
+        public override void End() {}
+
+        public override bool IsNull()
+            => HasValue;
+
+        public override DataReader NotNull()
+            => Reader;
+    }
+}
---
+++ Serialization/Writer/BinaryDataWriter.cs
@@ -0,0 +1,77 @@
+namespace Greenhouse.Libs.Serialization.Writer;
+
+public record BinaryDataWriter(BinaryWriter Binary) : DataWriter {
+    public ArrayDataWriter Array(int length) {
+        Primitive().Int(length);
+        return new ArrayWriter(this);
+    }
+
+    public ObjectDataWriter Object(int keys) {
+        return new ObjectWriter(this);
+    }
+
+    public PrimitiveDataWriter Primitive() => throw new NotImplementedException();
+
+    private record PrimitiveWriter(BinaryWriter Binary) : PrimitiveDataWriter {
+        public void Bool(bool value)
+            => Binary.Write(value);
+        public void Byte(byte value)
+            => Binary.Write(value);
+        public void Char(char value)
+            => Binary.Write(value);
+        public void Double(double value)
+            => Binary.Write(value);
+        public void Float(float value)
+            => Binary.Write(value);
+        public void Int(int value)
+            => Binary.Write(value);
+        public void Long(long value)
+            => Binary.Write(value);
+        public void SByte(sbyte value)
+            => Binary.Write(value);
+        public void Short(short value)
+            => Binary.Write(value);
+        public void String(string value)
+            => Binary.Write(value);
+        public void UInt(uint value)
+            => Binary.Write(value);
+        public void ULong(ulong value)
+            => Binary.Write(value);
+        public void UShort(ushort value)
+            => Binary.Write(value);
+    }
+
+    private class ArrayWriter(BinaryDataWriter writer) : ArrayDataWriter {
+        private readonly BinaryDataWriter Writer = writer;
+        public override void End() {}
+
+        public override DataWriter Value()
+            => Writer;
+    }
+
+    private class ObjectWriter(BinaryDataWriter writer) : ObjectDataWriter {
+        private readonly BinaryDataWriter Writer = writer;
+
+        public override void End() {}
+
+        public override DataWriter Field(string name)
+            => Writer;
+
+        public override NullableFieldDataWriter NullableField(string name)
+            => new NullableFieldWriter(Writer);
+    }
+
+    private class NullableFieldWriter(BinaryDataWriter writer) : NullableFieldDataWriter {
+        private readonly BinaryDataWriter Writer = writer;
+
+        public override void End() {}
+
+        public override DataWriter NotNull() {
+            Writer.Primitive().Bool(true);
+            return Writer;
+        }
+        public override void Null() {
+            Writer.Primitive().Bool(false);
+        }
+    }
+}
